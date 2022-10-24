5754bc7df11e6cc7901118db2cdfd0e70cb0433f
kalmuroth <lucas.bordner@cloudartifact.com>
2022-10-24 11:18:31 +0200

    fixes

--- streamlit/data.py
+++ streamlit/data.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import os
 import gc
 
-read = pd.read_csv("./sales_predictions.csv").sample(n=50, random_state=1)
+read = pd.read_csv("sales_predictions.csv").sample(n=50, random_state=1)
 
 #read = pd.to_datetime(read["date"], format='%d%m%Y')
 test = sns.relplot(data=read, x="date", y="item_price")
