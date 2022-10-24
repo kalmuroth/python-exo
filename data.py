f6cf35617048f91d7337f8240102e6d3801d6813
kalmuroth <lucas.bordner@cloudartifact.com>
2022-10-24 10:49:19 +0200

    absolute path defined

--- data.py
+++ data.py
@@ -4,7 +4,9 @@ import seaborn as sns
 import os
 import gc
 
-read = pd.read_csv("C:/Users/lucas/OneDrive/Bureau/serpent/input/sales_predictions.csv").sample(n=50, random_state=1)
+path = os.path.abspath("input/sales_predictions.csv")
+
+read = pd.read_csv(path).sample(n=50, random_state=1)
 #read = pd.to_datetime(read["date"], format='%d%m%Y')
 test = sns.relplot(data=read, x="date", y="item_price")
 print(test)
