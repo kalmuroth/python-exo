4f2b8ca5b773b81d203cd28405976550ff3b3241
kalmuroth <lucas.bordner@cloudartifact.com>
2022-10-24 11:30:24 +0200

    fixes

---
+++ data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import os
+import gc
+
+read = pd.read_csv("sales_predictions.csv").sample(n=50, random_state=1)
+
+#read = pd.to_datetime(read["date"], format='%d%m%Y')
+test = sns.relplot(data=read, x="date", y="item_price")
+print(test)
