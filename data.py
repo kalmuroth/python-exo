b16e3be4460671452bb758da104d5f09f197113f
kalmuroth <lucas.bordner@cloudartifact.com>
2022-10-24 11:38:48 +0200

    streamlit pyplot

--- data.py
+++ data.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import streamlit as st
 import os
 import gc
 
 read = pd.read_csv("sales_predictions.csv").sample(n=50, random_state=1)
-
-#read = pd.to_datetime(read["date"], format='%d%m%Y')
 test = sns.relplot(data=read, x="date", y="item_price")
-print(test)
+st.pyplot(test)
